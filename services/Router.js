110abb1fd989696dc1853eb988bdd2d9412c5f14
devOn-devEloper-fullstack <devon.cotter42@gmail.com>
2023-11-08 22:20:15 -0500

    Developed Routing capabilities, store implementation, and API
    global state management & facilitation abilities via store development, routing capabilities added for application navigation, and setup API for fetching application data

---
+++ services/Router.js
@@ -0,0 +1,56 @@
+const Router = {
+    init: () => {
+        document.querySelectorAll("a.navlink").forEach(a => {
+            a.addEventListener("click", event => {
+                event.preventDefault(); // Turn off refresh behavior
+                // Getting the URL from anchor tags with multiple options:
+                // const url = event.target.href
+                const url = event.target.getAttribute('href');
+                Router.go(url);
+            });
+        })
+        // Event Handler for URL changes:
+        window.addEventListener("popstate", event => {
+            Router.go(event.state.route, false);
+        })
+        
+        // Check the initial URL:
+        Router.go(location.pathname);
+    },
+    go: (route, addToHistory=true) => {
+        console.log(`Going to: ${route}`);
+
+        if (addToHistory) {
+            history.pushState({ route }, null, route);
+        }
+
+        let pageElement = null;
+        switch (route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu";
+                break;
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Your Order";
+                break;
+            default:
+                if (route.startsWith("/product-")) {
+                    pageElement = document.createElement("h1");
+                    pageElement.innerHTML = "Details";
+
+                    const paramId = route.substring(route.lastIndexOf('-')+1);
+                    pageElement.dataset.id = paramId;
+                }
+        }
+        if (pageElement){
+            document.querySelector("main").innerHTML = "";
+            document.querySelector("main").appendChild(pageElement);
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
+        
+    }
+}
+
+export default Router;
---
+++ services/Store.js
@@ -0,0 +1,8 @@
+// Used to manage state and provide a global store to the entire app.
+
+const Store = {
+    menu: null,
+    cart: []
+};
+
+export default Store;
