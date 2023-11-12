69cab20d23fa3f48d8b1caf5b38f43c76777e2f4
devOn-devEloper-fullstack <devon.cotter42@gmail.com>
2023-11-12 16:38:29 -0500

    Finalized app features and implemented reactivity to the order system

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import API from './services/API.js';
 import { loadData } from './services/Menu.js';
 import Router from './services/Router.js';
 
+// Web Components
+import { MenuPage } from './components/MenuPage.js';
+import { DetailsPage } from './components/DetailsPage.js';
+import { OrderPage } from './components/OrderPage.js';
+import { ProductItem } from './components/ProductItem.js';
+import { CartItem } from './components/CartItem.js';
+
 window.app = {}
 app.store = Store;
 app.router = Router;
@@ -10,4 +17,11 @@ app.router = Router;
 window.addEventListener("DOMContentLoaded", async () => {
     loadData();
     app.router.init();
+});
+
+window.addEventListener("appcartchange", event => {
+    const badge = document.getElementById("badge");
+    const qty = app.store.cart.reduce((acc, item) => {acc + item.quantity, 0});
+    badge.textContent = qty.quantity;
+    badge.hidden = qty == 0;
 });
